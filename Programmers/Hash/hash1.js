e4639da63cd7ba7b44aa43a2c1a97cf95b4721a8
hyesunie <hyesunie1025@gmail.com>
2021-04-07 10:52:38 +0900

    Programmers Hash 1

---
+++ Programmers/Hash/hash1.js
@@ -0,0 +1,24 @@
+function solution(participant, completion) {
+  let answer = "";
+
+  participant.sort();
+  completion.sort();
+
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) {
+      answer = participant[i];
+      return answer;
+    }
+  }
+}
+
+console.log(solution(["leo", "kiki", "eden"], ["eden", "kiki"]));
+console.log(
+  solution(
+    ["marina", "josipa", "nikola", "vinko", "filipa"],
+    ["josipa", "filipa", "marina", "nikola"]
+  )
+);
+console.log(
+  solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
+);
