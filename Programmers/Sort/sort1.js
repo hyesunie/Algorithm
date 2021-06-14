7f1492697f4ace7009e7a40e433d9728c86de4c5
hyesunie <hyesunie1025@gmail.com>
2021-06-14 21:47:48 +0900

    Programmers Sort1_2

--- Programmers/Sort/sort1.js
+++ Programmers/Sort/sort1.js
@@ -1,16 +1,32 @@
+// function solution(array, commands) {
+//   let answer = [];
+
+//   for (let key in commands) {
+//     let i = commands[key][0];
+//     let j = commands[key][1];
+//     let k = commands[key][2];
+
+//     let result = array.slice(i - 1, j);
+
+//     if (result.length > 1) result.sort((a, b) => a - b);
+
+//     answer.push(result[k - 1]);
+//   }
+
+//   console.log(answer);
+// }
+
 function solution(array, commands) {
   let answer = [];
+  let sliceArray = [];
 
-  for (let key in commands) {
-    let i = commands[key][0];
-    let j = commands[key][1];
-    let k = commands[key][2];
+  while (commands.length !== 0) {
+    let command = commands.shift();
+    let i = command[0] - 1;
+    let j = command[1];
+    let k = command[2] - 1;
 
-    let result = array.slice(i - 1, j);
-
-    if (result.length > 1) result.sort((a, b) => a - b);
-
-    answer.push(result[k - 1]);
+    answer.push(array.slice(i, j).sort((a, b) => a - b)[k]);
   }
 
   console.log(answer);
